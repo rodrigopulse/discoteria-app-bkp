e400282a1efa81aa96324a37958c4965fb8b3673
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-04-17 19:32:37 -0300

    colocada a cor primária hover

--- src/assets/styles/cores.js
+++ src/assets/styles/cores.js
@@ -1,6 +1,6 @@
 const Cores = {
   "corPrimaria": "#74F59A",
-  "corPrimariaHover": "#08720D",
+  "corPrimariaHover": "#62D684",
   "corSecundaria": "#21242D",
   "brancoHover": "#D6D6D6",
   "corErro": "#E54C4C",
