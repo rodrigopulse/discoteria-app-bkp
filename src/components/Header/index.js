cb659f904440299c64b9065b5cfd888ca2f5f929
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-04-20 18:54:13 -0300

    cor no botão sair

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -45,9 +45,25 @@ class Header extends React.Component {
                   source={require('../../assets/images/icons/baseline_cancel_black_18dp.png')}/>
               </TouchableHighlight>
               <TouchableHighlight
+                style={styles.itemMenu}>
+                <Text>Meu Perfil</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                style={styles.itemMenu}>
+                <Text>Minha Coleção</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                style={styles.itemMenu}>
+                <Text>Lista de Desejos</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                style={styles.itemMenu}>
+                <Text>Adicionar Disco</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
                 style={styles.itemMenu}
                 onPress = { () => { this.deslogar() }}>
-                <Text>Sair</Text>
+                <Text style={styles.botaoSair}>Sair</Text>
               </TouchableHighlight>
             </View>
           </View>
@@ -72,7 +88,6 @@ const styles = StyleSheet.create({
   menu: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-    //backgroundColor: "#000",
     position: "absolute",
     left: 0,
     top: 0,
@@ -83,15 +98,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingTop: 10,
     paddingLeft: 20,
-    paddingRight: 20,
+    paddingRight: 10,
     position: "absolute",
     left: 0,
     top: 0,
     zIndex: 2
   },
   itemMenu: {
-    paddingTop: 10,
-    paddingBottom: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  botaoSair: {
+    color: Cores.corErro,
   },
   botaoFechar: {
     alignSelf: "flex-end"
